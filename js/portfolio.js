d6653d81c84553c7b6e737977268f2aa0fafc760
justeldev <eluthmaaniy@gmail.com>
2025-10-04 10:24:46 +0100

    mayus update

---
+++ js/portfolio.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper', {
+            slidesPerView: 2,
+            spaceBetween: 16,
+            breakpoints: {
+                768: {
+                    slidesPerView: 'auto',
+                    spaceBetween: 24,
+                }
+            }
+        });
